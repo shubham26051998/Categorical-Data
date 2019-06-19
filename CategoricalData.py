7e21626ce054ce2987968e78674f1674506a98c6
Shubham Gupta <shubham26051998@gmail.com>
2019-06-20 00:25:33 +0530

    Add files via upload

---
+++ CategoricalData.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+# Load the data from a .csv file
+raw_data = pd.read_csv('1.03. Dummies.csv')
+
+# We copied to not change the data in the raw_data dataframe
+# Map all 'No' entries with 0, and all 'Yes' entries with 1. Put that in a new variable called 'data'
+data = raw_data.copy()
+data['Attendance'] = data['Attendance'].map({'Yes': 1, 'No': 0})
+
+# Following the regression equation, our dependent variable (y) is the GPA
+y = data ['GPA']
+# Similarly, our independent variable (x) is the SAT score
+x1 = data [['SAT','Attendance']]
+
+x = sm.add_constant(x1)
+# Fit the model, according to the OLS (ordinary least squares) method with a dependent variable y and an idependent x
+results = sm.OLS(y,x).fit()
+# Print summary of the regression.
+# results.summary()    -----> commented to run the program otherwise we have to first see the summary.
+
+
+plt.scatter(data['SAT'],data['GPA'], c=data['Attendance'],cmap='RdYlGn_r')
+
+# Define the two regression equations (one with a dummy = 1, the other with dummy = 0)
+# We have those above already, but for the sake of consistency, we will also include them here
+yhat_no = 0.6439 + 0.0014*data['SAT']
+yhat_yes = 0.8665 + 0.0014*data['SAT']
+# Original regression line
+yhat = 0.0017*data['SAT'] + 0.275
+
+# Plot the two regression lines
+fig = plt.plot(data['SAT'],yhat_no, lw=2, c='#006837', label ='regression line1')
+fig = plt.plot(data['SAT'],yhat_yes, lw=2, c='#a50026', label ='regression line2')
+# Plot the original regression line
+fig = plt.plot(data['SAT'],yhat, lw=3, c='#4C72B0', label ='regression line')
+
+plt.xlabel('SAT', fontsize = 20)
+plt.ylabel('GPA', fontsize = 20)
+plt.show()
+
